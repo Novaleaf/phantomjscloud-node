4f280700a43b0dbcb87cceba11214579406c747d
Jason <JasonS@Novaleaf.com>
2016-05-03 06:14:03 -0700

    remove typings from project

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/axios/index.d.ts" />
-/// <reference path="browser/ambient/mocha/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/definitions/bluebird/index.d.ts" />
-/// <reference path="browser/definitions/lodash/index.d.ts" />
